6cc6b8d7c8347d9f6dacb47d0e62706d672135af
BrianDYKim <18k7102dy@naver.com>
2022-06-02 23:31:53 +0900

    ShopReviewRepository.kt 작성 완료 및 테스트 완료

--- domain-dynamo/src/main/kotlin/team/bakkas/domaindynamo/repository/ShopReviewRepository.kt
+++ domain-dynamo/src/main/kotlin/team/bakkas/domaindynamo/repository/ShopReviewRepository.kt
@@ -2,14 +2,17 @@ package team.bakkas.domaindynamo.repository
 
 import org.springframework.stereotype.Repository
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
+import software.amazon.awssdk.enhanced.dynamodb.Expression
 import software.amazon.awssdk.enhanced.dynamodb.Key
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 import team.bakkas.domaindynamo.entity.ShopReview
 
 /** shop_review 테이블에 대한 repository class
  * @param dynamoDbEnhancedClient
  * @since 22/06/02
  * @author Brian
+ * @see <a href="https://github.com/awsdocs/aws-doc-sdk-examples/tree/main/kotlin/services/dynamodb#code-examples">Query method with Global Secondary Index</a>
  */
 @Repository
 class ShopReviewRepository(
@@ -36,6 +39,39 @@ class ShopReviewRepository(
         val reviewKey = generateKey(reviewId, reviewTitle)
 
         return table.getItem(reviewKey) ?: null
+    }
+
+    /** shopId와 shopName을 기반으로 특정 shop에 달려있는 모든 review를 가져오는 메소드
+     * @param shopId shop의 고유 id
+     * @param shopName shop의 이름
+     */
+    fun getReviewListByShopGsi(shopId: String, shopName: String): List<ShopReview> {
+        // filter expression에서 조건에 해당하는 변수명을 저장하는 map
+        val attributeAliasMap = mutableMapOf<String, String>()
+        // filter expression에서 값들을 저장하는 map
+        val attributeValueMap = mutableMapOf<String, AttributeValue>()
+
+        // Alias 식을 이용하여 shop_id를 표현
+        attributeAliasMap["#shop_id"] = "shop_id"
+        // Alias 식을 이용하여 shop_name을 표현
+        attributeAliasMap["#shop_name"] = "shop_name"
+
+        // 파라미터로 들어온 id를 저장
+        attributeValueMap[":id_val"] = AttributeValue.fromS(shopId)
+        // 파라미터로 들어온 name을 저장
+        attributeValueMap[":name_val"] = AttributeValue.fromS(shopName)
+
+        // expression 정의
+        val expression = Expression.builder()
+            .expressionNames(attributeAliasMap)
+            .expressionValues(attributeValueMap)
+            .expression("#shop_id = :id_val AND #shop_name = :name_val")
+            .build()
+
+        // scan 방식으로 테이블을 full-read 하여 조건에 해당하는 리뷰들을 리스트로 반환
+        return table.scan {
+            it.filterExpression(expression)
+        }.items().toList()
     }
 
     /** reviewId와 reviewTitle을 기반으로 review를 하나 삭제하는
--- domain-dynamo/src/test/kotlin/team/bakkas/domaindynamo/repository/ShopReviewRepositoryTest.kt
+++ domain-dynamo/src/test/kotlin/team/bakkas/domaindynamo/repository/ShopReviewRepositoryTest.kt
@@ -9,11 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.Rollback
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
+import software.amazon.awssdk.enhanced.dynamodb.Expression
 import software.amazon.awssdk.enhanced.dynamodb.Key
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 import team.bakkas.domaindynamo.entity.ShopReview
 import java.util.*
 
+/** ShopReviewRepository에 대한 Test Class.
+ * @author Brian
+ * @since 22/06/02
+ * @see <a href="https://github.com/awsdocs/aws-doc-sdk-examples/tree/main/kotlin/services/dynamodb#code-examples">Query method with Global Secondary Index</a>
+ */
 @SpringBootTest
 internal class ShopReviewRepositoryTest @Autowired constructor(
     private val dynamoDbEnhancedClient: DynamoDbEnhancedClient,
@@ -35,7 +42,7 @@ internal class ShopReviewRepositoryTest @Autowired constructor(
     }
 
     @ParameterizedTest
-    @CsvSource(value = ["ecdc2-test001:대만족이에요!:33daf043-7f36-4a52-b791-018f9d5eb218:역전할머니맥주 영남대점"], delimiter = ':')
+    @CsvSource(value = ["ecdc2-test001:킹킹만족!:33daf043-7f36-4a52-b791-018f9d5eb218:역전할머니맥주 영남대점"], delimiter = ':')
     @DisplayName("repository을 이용해서 데이터를 넣어보자!")
     @Rollback(value = false)
     fun createShopWithRepository(reviewId: String, reviewTitle: String, shopId: String, shopName: String) {
@@ -162,6 +169,7 @@ internal class ShopReviewRepositoryTest @Autowired constructor(
     }
 
     // ============================== [find all] ==============================
+
     @Test
     @DisplayName("client를 이용해서 모든 review 목록을 가져온다")
     fun clientFindAllReviews() {
@@ -170,7 +178,104 @@ internal class ShopReviewRepositoryTest @Autowired constructor(
         println(reviewList)
     }
 
-    // TODO delete test
+    // ============================== [findReviewsByShop] ==============================
+
+    @ParameterizedTest
+    @CsvSource(value = ["33daf043-7f36-4a52-b791-018f9d5eb218:역전할머니맥주 영남대점"], delimiter = ':')
+    @DisplayName("client를 이용해서 Shop에 대한 리뷰의 목록을 가져오는 로직 테스트.(Success)")
+    fun clientReviewListByShopGsi1(shopId: String, shopName: String) {
+
+        // given
+        val attributeAliasMap = mutableMapOf<String, String>()
+        attributeAliasMap["#shop_id"] = "shop_id"
+        attributeAliasMap["#shop_name"] = "shop_name"
+
+        val attributeValueMap = mutableMapOf<String, AttributeValue>()
+        attributeValueMap[":id_val"] = AttributeValue.fromS(shopId)
+        attributeValueMap[":name_val"] = AttributeValue.fromS(shopName)
+
+        // when
+        val expression = Expression.builder()
+            .expressionNames(attributeAliasMap)
+            .expressionValues(attributeValueMap)
+            .expression("#shop_id = :id_val AND #shop_name = :name_val")
+            .build()
+
+        // then
+        table.scan {
+            it.filterExpression(expression)
+        }.items().forEach { it -> println(it) }
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["nullnullnull:뭐가 없는 가게"], delimiter = ':')
+    @DisplayName("client를 이용해서 Shop에 대한 리뷰의 목록을 가져오는 로직 테스트.(count = 0)")
+    fun clientReviewListByShopGsi2(shopId: String, shopName: String) {
+        // given
+        var count = 0
+
+        val attributeAliasMap = mutableMapOf<String, String>()
+        attributeAliasMap["#shop_id"] = "shop_id"
+        attributeAliasMap["#shop_name"] = "shop_name"
+
+        val attributeValueMap = mutableMapOf<String, AttributeValue>()
+        attributeValueMap[":id_val"] = AttributeValue.fromS(shopId)
+        attributeValueMap[":name_val"] = AttributeValue.fromS(shopName)
+
+        // when
+        val expression = Expression.builder()
+            .expressionNames(attributeAliasMap)
+            .expressionValues(attributeValueMap)
+            .expression("#shop_id = :id_val AND #shop_name = :name_val")
+            .build()
+
+        table.scan {
+            it.filterExpression(expression)
+        }.items().forEach { it -> count++ }
+
+        // then
+        assertEquals(count, 0)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["33daf043-7f36-4a52-b791-018f9d5eb218:역전할머니맥주 영남대점"], delimiter = ':')
+    @DisplayName("repository를 이용해서 Shop에 대한 리뷰의 목록을 가져오는 로직 테스트.(Success)")
+    fun repositoryReviewListByShopGsi1(shopId: String, shopName: String) {
+        val reviewList = shopReviewRepository.getReviewListByShopGsi(shopId, shopName)
+
+        assertNotEquals(reviewList.size, 0)
+
+        reviewList.forEach { it -> println(it) }
+        println("Test passed!!")
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["nullnullnull:뭐가 없는 가게"], delimiter = ':')
+    @DisplayName("repository를 이용해서 Shop에 대한 리뷰의 목록을 가져오는 로직 테스트.(reviewList.size = 0)")
+    fun repositoryReviewListByShopGsi2(shopId: String, shopName: String) {
+        val reviewList = shopReviewRepository.getReviewListByShopGsi(shopId, shopName)
+
+        assertEquals(reviewList.size, 0)
+
+        println("Test passed!!")
+    }
+
+    // ============================== [deleteReview] ==============================
+
+    @ParameterizedTest
+    @CsvSource(value = ["c4633cf7-dca1-447c-907c-acb2f0db73df:킹킹만족!"], delimiter = ':')
+    @DisplayName("repository를 이용해서 리뷰 삭제 (Success)")
+    fun deleteReview(reviewId: String, reviewTitle: String) {
+        // when
+        shopReviewRepository.deleteReview(reviewId, reviewTitle)
+
+        // then
+        val deletedShop = shopReviewRepository.findReviewByIdAndTitle(reviewId, reviewTitle)
+
+        assertNull(deletedShop)
+
+        println("Test passed!!")
+    }
 
     // 키를 생성하는 메소드
     private fun generateKey(reviewId: String, reviewTitle: String): Key = Key.builder()
