299fbd7fb39017b9bd9100f5e202849cc4ada087
BrianDYKim <18k7102dy@naver.com>
2022-06-02 17:16:07 +0900

    ShopReviewRepository.kt 작성중

--- domain-dynamo/src/main/kotlin/team/bakkas/domaindynamo/repository/ShopReviewRepository.kt
+++ domain-dynamo/src/main/kotlin/team/bakkas/domaindynamo/repository/ShopReviewRepository.kt
@@ -18,6 +18,36 @@ class ShopReviewRepository(
     // shop_review에 대한 테이블 정의
     val table = dynamoDbEnhancedClient.table("shop_review", TableSchema.fromBean(ShopReview::class.java))
 
+    /** Shop에 대한 Review를 생성하는 메소드
+     * @param shopReview 리뷰 엔티티
+     */
+    fun createReview(shopReview: ShopReview): ShopReview {
+        table.putItem(shopReview)
+
+        return shopReview
+    }
+
+    /** reviewId와 reviewTitle을 기반으로 review를 하나 찾아오는 메소드
+     * @param reviewId
+     * @param reviewTitle
+     * @return ShopReview if exists else null
+     */
+    fun findReviewByIdAndTitle(reviewId: String, reviewTitle: String): ShopReview? {
+        val reviewKey = generateKey(reviewId, reviewTitle)
+
+        return table.getItem(reviewKey) ?: null
+    }
+
+    /** reviewId와 reviewTitle을 기반으로 review를 하나 삭제하는
+     * @param reviewId
+     * @param reviewTitle
+     */
+    fun deleteReview(reviewId: String, reviewTitle: String) {
+        val reviewKey = generateKey(reviewId, reviewTitle)
+
+        table.deleteItem(reviewKey)
+    }
+
     /** Key를 반환하는 private method
      * @param reviewId Partition Key of shop_review
      * @param reviewTitle Sort Key of shop_review
--- domain-dynamo/src/test/kotlin/team/bakkas/domaindynamo/repository/ShopReviewRepositoryTest.kt
+++ domain-dynamo/src/test/kotlin/team/bakkas/domaindynamo/repository/ShopReviewRepositoryTest.kt
@@ -1,30 +1,181 @@
 package team.bakkas.domaindynamo.repository
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.Rollback
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable
 import software.amazon.awssdk.enhanced.dynamodb.Key
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema
-import team.bakkas.domaindynamo.entity.Shop
 import team.bakkas.domaindynamo.entity.ShopReview
+import java.util.*
 
 @SpringBootTest
 internal class ShopReviewRepositoryTest @Autowired constructor(
-    private val dynamoDbEnhancedClient: DynamoDbEnhancedClient
+    private val dynamoDbEnhancedClient: DynamoDbEnhancedClient,
+    private val shopReviewRepository: ShopReviewRepository
 ) {
     // 테이블 정의
-    val table: DynamoDbTable<Shop> = dynamoDbEnhancedClient.table("shop", TableSchema.fromBean(Shop::class.java))
+    val table = dynamoDbEnhancedClient.table("shop_review", TableSchema.fromBean(ShopReview::class.java))
 
-    fun createShopWithClient() {
+    // ============================== [create] ==============================
 
+    @ParameterizedTest
+    @CsvSource(value = ["ecdc2-test001:대만족이에요!:33daf043-7f36-4a52-b791-018f9d5eb218:역전할머니맥주 영남대점"], delimiter = ':')
+    @DisplayName("table을 이용해서 데이터를 넣어보자!")
+    @Rollback(value = false)
+    fun createShopWithClient(reviewId: String, reviewTitle: String, shopId: String, shopName: String) {
+        val mockReview = getMockReview(reviewId, reviewTitle, shopId, shopName)
+
+        table.putItem(mockReview)
     }
 
+    @ParameterizedTest
+    @CsvSource(value = ["ecdc2-test001:대만족이에요!:33daf043-7f36-4a52-b791-018f9d5eb218:역전할머니맥주 영남대점"], delimiter = ':')
+    @DisplayName("repository을 이용해서 데이터를 넣어보자!")
+    @Rollback(value = false)
+    fun createShopWithRepository(reviewId: String, reviewTitle: String, shopId: String, shopName: String) {
+        // given
+        val mockReview = getMockReview(UUID.randomUUID().toString(), reviewTitle, shopId, shopName)
+
+        // when
+        val createdReview = shopReviewRepository.createReview(mockReview)
+
+        // then
+        with(createdReview) {
+            assertNotNull(this)
+            assertEquals(reviewTitle, this.reviewTitle)
+            assertEquals(shopId, this.shopId)
+            assertEquals(shopName, this.shopName)
+        }
+
+        println("Test passed!!")
+    }
+
+    // ============================== [find] ==============================
+
+    @ParameterizedTest
+    @CsvSource(value = ["6274cfba-92a7-4e44-9a8f-123113e928bc:대만족이에요!"], delimiter = ':')
+    @DisplayName("client를 이용해서 find 테스트. (Success)")
+    fun clientSuccessFindReviewWithIdAndName(reviewId: String, reviewTitle: String) {
+        // given
+        val reviewKey = generateKey(reviewId, reviewTitle)
+
+        // when
+        val foundReview = table.getItem(reviewKey)
+
+        // then
+        with(foundReview) {
+            assertNotNull(this)
+            assertEquals(reviewId, this.reviewId)
+            assertEquals(reviewTitle, this.reviewTitle)
+        }
+
+        println("Test passed!!")
+        println(foundReview)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["가짜아이디:대만족이에요!"], delimiter = ':')
+    @DisplayName("client를 이용해서 find 테스트. reviewId를 잘못 주는 케이스(Fail)")
+    fun clientFailureFindReviewWithIdAndName1(reviewId: String, reviewTitle: String) {
+        // given
+        val reviewKey = generateKey(reviewId, reviewTitle)
+
+        // when
+        val foundReview = table.getItem(reviewKey)
+
+        // then
+        with(foundReview) {
+            assertNull(this)
+        }
+
+        println("Test passed!!")
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["6274cfba-92a7-4e44-9a8f-123113e928bc:불만족이에요!"], delimiter = ':')
+    @DisplayName("client를 이용해서 find 테스트. reviewTitle를 잘못 주는 케이스(Fail)")
+    fun clientFailureFindReviewWithIdAndName2(reviewId: String, reviewTitle: String) {
+        // given
+        val reviewKey = generateKey(reviewId, reviewTitle)
+
+        // when
+        val foundReview = table.getItem(reviewKey)
+
+        // then
+        with(foundReview) {
+            assertNull(this)
+        }
+
+        println("Test passed!!")
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["6274cfba-92a7-4e44-9a8f-123113e928bc:대만족이에요!"], delimiter = ':')
+    @DisplayName("repository를 이용해서 find 테스트. (Success)")
+    fun repositorySuccessFindReviewWithIdAndTitle(reviewId: String, reviewTitle: String) {
+        // when
+        val foundReview = shopReviewRepository.findReviewByIdAndTitle(reviewId, reviewTitle)
+
+        // then
+        with(foundReview) {
+            assertNotNull(this)
+            assertEquals(this!!.reviewId, reviewId)
+            assertEquals(this!!.reviewTitle, reviewTitle)
+        }
+
+        println(foundReview)
+        println("Test passed!!")
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["가짜아이디:대만족이에요!"], delimiter = ':')
+    @DisplayName("repository를 이용해서 find 테스트. reviewId를 잘못 주는 케이스(Fail)")
+    fun repositoryFailureFindReviewWithIdAndTitle1(reviewId: String, reviewTitle: String) {
+        // when
+        val foundReview = shopReviewRepository.findReviewByIdAndTitle(reviewId, reviewTitle)
+
+        with(foundReview) {
+            assertNull(this)
+        }
+
+        println("Test passed!!")
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["6274cfba-92a7-4e44-9a8f-123113e928bc:불만족이에요!"], delimiter = ':')
+    @DisplayName("repository를 이용해서 find 테스트. reviewTitle를 잘못 주는 케이스(Fail)")
+    fun repositoryFailureFindReviewWithIdAndTitle2(reviewId: String, reviewTitle: String) {
+        // when
+        val foundReview = shopReviewRepository.findReviewByIdAndTitle(reviewId, reviewTitle)
+
+        with(foundReview) {
+            assertNull(this)
+        }
+
+        println("Test passed!!")
+    }
+
+    // ============================== [find all] ==============================
+    @Test
+    @DisplayName("client를 이용해서 모든 review 목록을 가져온다")
+    fun clientFindAllReviews() {
+        val reviewList = table.scan().items().toList()
+
+        println(reviewList)
+    }
+
+    // TODO delete test
+
     // 키를 생성하는 메소드
-    private fun generateKey(reviewId: String, reviewName: String): Key = Key.builder()
+    private fun generateKey(reviewId: String, reviewTitle: String): Key = Key.builder()
         .partitionValue(reviewId)
-        .sortValue(reviewName)
+        .sortValue(reviewTitle)
         .build()
 
     // 리뷰를 하나 생성하는 메소드
