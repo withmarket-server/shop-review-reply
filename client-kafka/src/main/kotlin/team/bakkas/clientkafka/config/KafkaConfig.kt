8467d816877b5d8d0a3916ff917b30e1da021f7b
BrianDYKim <18k7102dy@naver.com>
2022-07-25 19:05:54 +0900

    kafka config 작성중

---
+++ client-kafka/src/main/kotlin/team/bakkas/clientkafka/config/KafkaConfig.kt
@@ -0,0 +1,13 @@
+package team.bakkas.clientkafka.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.ProducerFactory
+
+/**
+ *
+ */
+@Configuration
+class KafkaConfig {
+
+
+}
