50339ad6b4ee8dcbcbf53c1158d336458c5d28f6
BrianDYKim <18k7102dy@naver.com>
2022-10-16 23:26:10 +0900

    [ADD] (adapter:router-query) gRPC Service들에 대한 단위테스트 완료

--- adapter/router-query/src/test/kotlin/team/bakkas/applicationquery/grpc/GrpcShopReviewServiceUnitTest.kt
+++ adapter/router-query/src/test/kotlin/team/bakkas/applicationquery/grpc/GrpcShopReviewServiceUnitTest.kt
@@ -1,12 +1,20 @@
 package team.bakkas.applicationquery.grpc
 
+import io.kotest.common.runBlocking
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
 import io.mockk.spyk
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import team.bakkas.domainquery.service.ifs.ShopReviewQueryService
+import team.bakkas.dynamo.shopReview.ShopReview
+import team.bakkas.grpcIfs.v1.shopReview.CheckExistShopReviewRequest
+import java.time.LocalDateTime
 
 // gRPC ShopReview Service에 대한 단위테스트
 @ExtendWith(MockKExtension::class)
@@ -20,4 +28,67 @@ internal class GrpcShopReviewServiceUnitTest {
         shopReviewQueryService = mockk(relaxed = true)
         grpcShopReviewService = spyk(GrpcShopReviewService(shopReviewQueryService))
     }
+
+    @Test
+    @DisplayName("[isExistShopReview] Review가 존재하지 않는 경우")
+    fun isExistShopReview1(): Unit = runBlocking {
+        // given
+        val reviewId = "1"
+        val reviewTitle = "review1"
+        val shopId = "1"
+        val shopName = "shop1"
+        val request = CheckExistShopReviewRequest.newBuilder()
+            .setReviewId(reviewId)
+            .setReviewTitle(reviewTitle)
+            .build()
+
+        coEvery { shopReviewQueryService.findReviewByIdAndTitle(reviewId, reviewTitle) } returns null
+
+        // when
+        val response = grpcShopReviewService.isExistShopReview(request)
+
+        // then
+        coVerify(exactly = 1) { grpcShopReviewService.isExistShopReview(request) }
+        coVerify(exactly = 1) { shopReviewQueryService.findReviewByIdAndTitle(reviewId, reviewTitle) }
+        assertEquals(response.result, false)
+    }
+
+    @Test
+    @DisplayName("[isExistShopReview] Review가 존재하는 경우 성공")
+    fun isExistShopReview2(): Unit = runBlocking {
+        // given
+        val reviewId = "1"
+        val reviewTitle = "review1"
+        val shopId = "1"
+        val shopName = "shop1"
+        val request = CheckExistShopReviewRequest.newBuilder()
+            .setReviewId(reviewId)
+            .setReviewTitle(reviewTitle)
+            .build()
+
+        coEvery { shopReviewQueryService.findReviewByIdAndTitle(reviewId, reviewTitle) } returns
+                getMockReview(reviewId, reviewTitle, shopId, shopName)
+
+        // when
+        val response = grpcShopReviewService.isExistShopReview(request)
+
+        // then
+        coVerify(exactly = 1) { grpcShopReviewService.isExistShopReview(request) }
+        coVerify(exactly = 1) { shopReviewQueryService.findReviewByIdAndTitle(reviewId, reviewTitle) }
+        assertEquals(response.result, true)
+    }
+
+    // 가짜 review를 반환하는 메소드
+    private fun getMockReview(reviewId: String, reviewTitle: String, shopId: String, shopName: String) =
+        ShopReview(
+            reviewId = reviewId,
+            reviewTitle = reviewTitle,
+            shopId = shopId,
+            shopName = shopName,
+            reviewContent = "저는 아주 불만족했어요! ^^",
+            reviewScore = 1.0,
+            reviewPhotoList = listOf(),
+            createdAt = LocalDateTime.now(),
+            updatedAt = null
+        )
 }
--- adapter/router-query/src/test/kotlin/team/bakkas/applicationquery/grpc/GrpcShopServiceUnitTest.kt
+++ adapter/router-query/src/test/kotlin/team/bakkas/applicationquery/grpc/GrpcShopServiceUnitTest.kt
@@ -1,12 +1,26 @@
 package team.bakkas.applicationquery.grpc
 
+import io.kotest.common.runBlocking
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
 import io.mockk.spyk
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import team.bakkas.domainquery.service.ifs.ShopQueryService
+import team.bakkas.dynamo.shop.Shop
+import team.bakkas.dynamo.shop.vo.*
+import team.bakkas.dynamo.shop.vo.category.Category
+import team.bakkas.dynamo.shop.vo.category.DetailCategory
+import team.bakkas.dynamo.shop.vo.sale.Days
+import team.bakkas.dynamo.shopReview.ShopReview
+import team.bakkas.grpcIfs.v1.shop.CheckExistShopRequest
+import java.time.LocalDateTime
+import java.time.LocalTime
 
 // gRPC shop service에 대한 단위테스트
 @ExtendWith(MockKExtension::class)
@@ -20,4 +34,72 @@ internal class GrpcShopServiceUnitTest {
         shopQueryService = mockk(relaxed = true)
         grpcShopService = spyk(GrpcShopService(shopQueryService))
     }
+
+    @Test
+    @DisplayName("[isExistShop] 1. Shop이 존재하지 않는 경우")
+    fun isExistShopTest1(): Unit = runBlocking {
+        // given
+        val shopId = "shopId"
+        val shopName = "shopName"
+        val request = CheckExistShopRequest.newBuilder()
+            .setShopId(shopId)
+            .setShopName(shopName)
+            .build()
+
+        coEvery { shopQueryService.findShopByIdAndName(shopId, shopName) } returns null
+
+        // when
+        val response = grpcShopService.isExistShop(request)
+
+        // then
+        coVerify(exactly = 1) { grpcShopService.isExistShop(request) }
+        coVerify(exactly = 1) { shopQueryService.findShopByIdAndName(shopId, shopName) }
+        assertEquals(response.result, false)
+    }
+
+    @Test
+    @DisplayName("[isExistShop] 2. Shop이 존재하는 경우")
+    fun isExistShopTest2(): Unit = runBlocking {
+        // given
+        val shopId = "shopId"
+        val shopName = "shopName"
+        val request = CheckExistShopRequest.newBuilder()
+            .setShopId(shopId)
+            .setShopName(shopName)
+            .build()
+
+        coEvery { shopQueryService.findShopByIdAndName(shopId, shopName) } returns
+                generateFakeShop(shopId, shopName)
+
+        // when
+        val response = grpcShopService.isExistShop(request)
+
+        // then
+        coVerify(exactly = 1) { grpcShopService.isExistShop(request) }
+        coVerify(exactly = 1) { shopQueryService.findShopByIdAndName(shopId, shopName) }
+        with(response) {
+            assertEquals(this.result, true)
+        }
+    }
+
+    // 테스트용 가짜 shop
+    private fun generateFakeShop(shopId: String, shopName: String) = Shop(
+        shopId = shopId,
+        shopName = shopName,
+        salesInfo = SalesInfo(
+            isOpen = false,
+            openTime = LocalTime.now(),
+            closeTime = LocalTime.now(),
+            restDayList = listOf(Days.SUN)
+        ),
+        addressInfo = AddressInfo("lotNumberAddress", "roadNameAddress"),
+        latLon = LatLon(128.0, 36.0),
+        shopImageInfo = ShopImageInfo("mainImageUrl", listOf("qwe")),
+        branchInfo = BranchInfo(false, null),
+        categoryInfo = CategoryInfo(Category.MART, DetailCategory.SUPER_MARKET),
+        deliveryTipPerDistanceList = listOf(DeliveryTipPerDistance(3.0, 3000)),
+        totalScore = 0.0,
+        reviewNumber = 0,
+        shopDescription = "shopDescription"
+    )
 }
