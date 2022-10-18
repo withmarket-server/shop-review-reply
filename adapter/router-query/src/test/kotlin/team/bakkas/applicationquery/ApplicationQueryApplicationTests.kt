9844c9c8e5710be9ddaef42b984c053658ee28cb
BrianDYKim <18k7102dy@naver.com>
2022-10-19 02:22:02 +0900

    [ADD] (ALL) relaxed 해제

--- adapter/router-query/src/test/kotlin/team/bakkas/applicationquery/ApplicationQueryApplicationTests.kt
+++ adapter/router-query/src/test/kotlin/team/bakkas/applicationquery/ApplicationQueryApplicationTests.kt
@@ -3,7 +3,7 @@ package team.bakkas.applicationquery
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+// @SpringBootTest
 class ApplicationQueryApplicationTests {
 
     @Test
