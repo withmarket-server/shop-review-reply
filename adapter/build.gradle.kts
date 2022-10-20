d4b5878f9041ea11f43c039f81c03f66a43c4ba5
BrianDYKim <18k7102dy@naver.com>
2022-10-20 18:33:20 +0900

    adapter 루트 모듈을 빌드 대상에서 제외

---
+++ adapter/build.gradle.kts
@@ -0,0 +1,10 @@
+// root folder의 gradle project는 빌드하지 않는다
+tasks {
+    named<Jar>("jar") {
+        enabled = true
+    }
+
+    named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+        enabled = false
+    }
+}
