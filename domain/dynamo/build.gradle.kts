bc4dd9dd37030d7654c07a29a81ea62cfdc40b7d
BrianDYKim <18k7102dy@naver.com>
2022-10-20 18:47:43 +0900

    domain:dynamo를 빌드 대상에서 제외

--- domain/dynamo/build.gradle.kts
+++ domain/dynamo/build.gradle.kts
@@ -19,3 +19,14 @@ noArg {
 tasks.withType<Test> {
     exclude("**/dynamo/**")
 }
+
+// root folder의 gradle project는 빌드하지 않는다
+tasks {
+    named<Jar>("jar") {
+        enabled = true
+    }
+
+    named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+        enabled = false
+    }
+}
