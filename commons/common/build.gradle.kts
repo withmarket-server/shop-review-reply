33deef82651716559a3f8fbc1d08289474706a6a
BrianDYKim <18k7102dy@naver.com>
2022-10-20 18:45:02 +0900

    commons:common을 빌드 대상에서 제외

--- commons/common/build.gradle.kts
+++ commons/common/build.gradle.kts
@@ -6,3 +6,14 @@ dependencies {
 tasks.withType<Test> {
     exclude("**/common/**")
 }
+
+// 해당 모듈은 빌드 대상에서 제외한다
+tasks {
+    named<Jar>("jar") {
+        enabled = true
+    }
+
+    named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+        enabled = false
+    }
+}
