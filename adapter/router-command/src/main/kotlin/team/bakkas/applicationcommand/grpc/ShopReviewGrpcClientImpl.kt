3c84a26b5157e40406cc77f87e8cf0c60aa95121
BrianDYKim <18k7102dy@naver.com>
2022-11-07 00:07:50 +0900

    [ADD] (adapter) grpc channel 설정

--- adapter/router-command/src/main/kotlin/team/bakkas/applicationcommand/grpc/ShopReviewGrpcClientImpl.kt
+++ adapter/router-command/src/main/kotlin/team/bakkas/applicationcommand/grpc/ShopReviewGrpcClientImpl.kt
@@ -1,6 +1,7 @@
 package team.bakkas.applicationcommand.grpc
 
 import io.grpc.ManagedChannelBuilder
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import team.bakkas.applicationcommand.grpc.ifs.ShopReviewGrpcClient
 import team.bakkas.grpcIfs.v1.shopReview.CheckExistShopReviewRequest
@@ -9,17 +10,17 @@ import team.bakkas.grpcIfs.v1.shopReview.ShopReviewServiceGrpcKt
 
 // ShopReview에 대한 gRPC 서비스를 스터빙하는 클래스
 @Component
-class ShopReviewGrpcClientImpl : ShopReviewGrpcClient {
+class ShopReviewGrpcClientImpl(
+    @Value("\${cluster.grpc-host}") private val channelHost: String
+) : ShopReviewGrpcClient {
 
-    companion object {
-        // query server를 대상으로 gRPC 포트를 타겟한다
-        val channel = ManagedChannelBuilder.forAddress("localhost", 10100)
-            .usePlaintext()
-            .build()
+    // query server를 대상으로 gRPC 포트를 타겟한다
+    private val channel = ManagedChannelBuilder.forAddress(channelHost, 10100)
+        .usePlaintext()
+        .build()
 
-        // stubbing 객체
-        val shopReviewStub = ShopReviewServiceGrpcKt.ShopReviewServiceCoroutineStub(channel)
-    }
+    // stubbing 객체
+    private val shopReviewStub = ShopReviewServiceGrpcKt.ShopReviewServiceCoroutineStub(channel)
 
     /** reviewId, reviewTitle에 대응하는 shopReview가 존재하는지 여부를 반환하는 메소드
      * @param reviewId
