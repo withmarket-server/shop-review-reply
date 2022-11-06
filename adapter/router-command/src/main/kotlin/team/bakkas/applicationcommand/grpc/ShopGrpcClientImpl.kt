52bf834ffae1caf862b85f409daed1b2a6f0c925
BrianDYKim <18k7102dy@naver.com>
2022-11-07 00:06:44 +0900

    [ADD] (adapter) grpc channel 설정

--- adapter/router-command/src/main/kotlin/team/bakkas/applicationcommand/grpc/ShopGrpcClientImpl.kt
+++ adapter/router-command/src/main/kotlin/team/bakkas/applicationcommand/grpc/ShopGrpcClientImpl.kt
@@ -1,6 +1,7 @@
 package team.bakkas.applicationcommand.grpc
 
 import io.grpc.ManagedChannelBuilder
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import team.bakkas.applicationcommand.grpc.ifs.ShopGrpcClient
 import team.bakkas.grpcIfs.v1.shop.CheckExistShopRequest
@@ -9,17 +10,16 @@ import team.bakkas.grpcIfs.v1.shop.ShopServiceGrpcKt
 
 // Shop에 대한 Grpc 서비스를 스터빙하는 클래스
 @Component
-class ShopGrpcClientImpl : ShopGrpcClient {
+class ShopGrpcClientImpl(
+    @Value("\${cluster.grpc-host}") private val channelHost: String
+) : ShopGrpcClient {
+    // query server를 대상으로 gRPC 포트를 타겟한다
+    private val channel = ManagedChannelBuilder.forAddress(channelHost, 10100)
+        .usePlaintext()
+        .build()
 
-    companion object {
-        // query server를 대상으로 gRPC 포트를 타겟한다
-        val channel = ManagedChannelBuilder.forAddress("localhost", 10100)
-            .usePlaintext()
-            .build()
-
-        // stubbing 객체
-        val shopStub = ShopServiceGrpcKt.ShopServiceCoroutineStub(channel)
-    }
+    // stubbing 객체
+    private val shopStub = ShopServiceGrpcKt.ShopServiceCoroutineStub(channel)
 
     /** shopId, shopName에 대응하는 shop이 존재하는지 여부를 반환해주는 메소드
      * @param shopId
