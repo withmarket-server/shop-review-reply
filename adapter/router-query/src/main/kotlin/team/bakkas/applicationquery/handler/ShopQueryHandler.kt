796d67fff43299e87da21887bfedc51b92f3314f
BrianDYKim <18k7102dy@naver.com>
2022-11-09 03:04:40 +0900

    [ADD] shopQueryHandler의 searchByCategoryWithIn 테스트 완료

--- adapter/router-query/src/main/kotlin/team/bakkas/applicationquery/handler/ShopQueryHandler.kt
+++ adapter/router-query/src/main/kotlin/team/bakkas/applicationquery/handler/ShopQueryHandler.kt
@@ -1,6 +1,10 @@
 package team.bakkas.applicationquery.handler
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.*
@@ -9,8 +13,10 @@ import team.bakkas.applicationquery.extensions.toSimpleResponse
 import team.bakkas.applicationquery.grpc.client.GrpcShopSearchClient
 import team.bakkas.common.ResultFactory
 import team.bakkas.common.exceptions.RequestParamLostException
+import team.bakkas.common.exceptions.shop.CategoryNotFoundException
 import team.bakkas.common.exceptions.shop.ShopNotFoundException
 import team.bakkas.domainquery.service.ifs.ShopQueryService
+import team.bakkas.dynamo.shop.vo.category.Category
 
 /** Shop에 대한 Query logic을 처리하는 Handler class
  * @param shopQueryService shop에 대한 Service 로직들을 저장한 컨포넌트
@@ -33,7 +39,7 @@ class ShopQueryHandler(
         // shop이 발견되지 않으면 ShopNotFoundException을 일으킨다
         val shop = shopQueryService.findShopById(shopId) ?: throw ShopNotFoundException("Shop is not found!!")
 
-        if(shop.deletedAt != null) {
+        if (shop.deletedAt != null) {
             throw ShopNotFoundException("Shop is not found!!")
         }
 
@@ -60,5 +66,41 @@ class ShopQueryHandler(
     }
 
     // TODO 반경 검색, 카테고리별 검색, 세부 카테고리별 검색, 가게 이름 기반 검색 구현
+    suspend fun searchByCategoryWithIn(request: ServerRequest): ServerResponse = coroutineScope {
+        val category = request.queryParamOrNull("category") ?: throw RequestParamLostException("category is lost")
+        val latitude = request.queryParamOrNull("latitude") ?: throw RequestParamLostException("latitude is lost")
+        val longitude = request.queryParamOrNull("longitude") ?: throw RequestParamLostException("longitude is lost")
+        val distance = request.queryParamOrNull("distance") ?: throw RequestParamLostException("distance is lost")
+        val unit = request.queryParamOrNull("unit") ?: throw RequestParamLostException("unit is lost")
+        val page = request.queryParamOrNull("page") ?: throw RequestParamLostException("page is lost")
+        val size = request.queryParamOrNull("size") ?: throw RequestParamLostException("size is lost")
 
+        // Check the given category is valid
+        if (category !in Category.values().map { it.toString() }) {
+            throw CategoryNotFoundException("Category is not valid")
+        }
+
+        val satisfiedShopIdFlow = grpcShopSearchClient.searchCategoryWIthIn(
+            category,
+            latitude.toDouble(),
+            longitude.toDouble(),
+            distance.toDouble(),
+            unit,
+            page.toInt(),
+            size.toInt()
+        ).idsList.asFlow()
+
+        val shopResponseList = satisfiedShopIdFlow
+            .map { shopQueryService.findShopById(it)!! }
+            .map { it.toSimpleResponse() } // 간략화된 shop 정보로 변환
+            .toList()
+
+        check(shopResponseList.isNotEmpty()) {
+            throw ShopNotFoundException("Shop is not found!!")
+        }
+
+        return@coroutineScope ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValueAndAwait(ResultFactory.getMultipleResult(shopResponseList))
+    }
 }
